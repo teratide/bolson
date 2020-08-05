b5b5053b7d18e2a01782fb4e667ae0d366b2e464
Johan Peltenburg <j.w.peltenburg@tudelft.nl>
2020-08-05 16:25:42 +0200

    Add latency plots

--- bench/single_batch_sweep_plot.py
+++ bench/single_batch_sweep_plot.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import math
+from textwrap import wrap
 
 plt.rcParams.update({"text.usetex": True})
-plt.rcParams.update({"font.size": '14'})
+plt.rcParams.update({"font.size": '12'})
 
 background = '#f5ebdf'
-line_colors = ['#000000', '#c0a040', '#007e80', '#e06055', '#3f5fbf', '#d275de', '#7bb144', '#888888']
+colors = ['#000000', '#c0a040', '#007e80', '#e06055', '#3f5fbf', '#d275de', '#7bb144', '#888888']
 markers = ['o', 's', 'd', 'v', '^', '<', '>']
 
 
@@ -24,7 +25,7 @@ def plot_throughput():
     ax.set_title('JSON $\\rightarrow$ Arrow $\\rightarrow$ Pulsar', fontweight="bold", fontsize=20)
 
     for (i, (name, data)) in enumerate(y.iteritems()):
-        ax.plot(x, data, color=line_colors[i], marker=markers[i], markersize=8)
+        ax.plot(x, data, color=colors[i], marker=markers[i], markersize=8)
         i = i + 1
 
     ax.set_xlabel(x.name)
@@ -57,20 +58,64 @@ def plot_throughput():
 
 def plot_latency():
     df = pd.read_csv("single_batch_sweep_result.csv")
-    x = df['Arrow IPC messages total size (B)']
-    y = df[['Load JSON (s)', 'Parse JSON (s)', 'Convert to Arrow RecordBatches (in) (s)',
+    b = df['Arrow IPC messages avg. size (B)']
+    t = df['Number of tweets']
+    x = t  # df['Number of RecordBatches']
+    y = df[['Load JSON (s)',
+            'Parse JSON (s)',
             'Convert to Arrow RecordBatches (out) (s)',
-            'Write Arrow IPC messages (s)', 'Publish IPC messages in Pulsar (s)']]
+            'Write Arrow IPC messages (s)',  # ]].div(t, axis='index')
+            'Publish IPC messages in Pulsar (s)']].div(t, axis='index')
+
+    pd.set_option('display.max_rows', 10)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', None)
+    pd.set_option('display.max_colwidth', None)
+
+    rows = y.shape[0]
 
     # Create figs and plot
-    fig = plt.figure(figsize=(8, 6))
-    ax = fig.add_subplot(1, 1, 1)
+    fig = plt.figure(figsize=(10, 8))
+    axes = fig.subplots(nrows=6, sharex=True)  # , gridspec_kw={'wspace': 0, 'hspace': 0.05})
 
-    ax.set_title('JSON $\\rightarrow$ Arrow $\\rightarrow$ Pulsar', fontweight="bold", fontsize=20)
+    for i in range(0, rows):
+        for (j, value) in enumerate(y.iloc[i].values):
+            axes[j].bar(x[i], value, width=0.4 * x[i], color=colors[j])
+            axes[j].text(x[i], value, '{:.2e}'.format(value),
+                         ha='left', va='bottom', rotation=30,
+                         fontsize='xx-small', fontstyle='oblique')
 
-    for (i, (name, data)) in enumerate(y.iteritems()):
-        ax.plot(x, data, color=line_colors[i], marker=markers[i], markersize=8)
-        i = i + 1
+        axes[5].bar(x[i], b[i], width=0.4 * x[i], color=colors[5])
+        axes[5].text(x[i], b[i], '{:.2e}'.format(b[i]),
+                     ha='left', va='bottom', rotation=30,
+                     fontsize='xx-small', fontstyle='oblique')
+
+    for (j, ax) in enumerate(axes[:5]):
+        axes[j].set_ylabel('\n'.join(wrap('Avg. ' + y.columns[j], 11)), rotation=0, ha='right', va='center', fontsize='x-small')
+        ax.set_xscale('log', base=2)
+        ax.set_xlim([1 / 2, x.max() * 2])
+        ax.set_xticks([2 ** x for x in range(0, int(math.ceil(math.log2(x.max() * 2))))])
+        ax.set_yscale('log', base=10)
+        ax.set_yticks([10 ** (-x) for x in range(1, 7)])
+        ax.set_ylim([10 ** (-7), 1])
+
+        ax.grid(which='both')
+        ax.set_facecolor(background)
+
+    axes[5].set_yscale('log', base=2)
+    axes[5].set_yticks([2 ** x for x in range(10, 26, 2)])
+    axes[5].set_ylim([b.min()/2, 16 * b.max()])
+    axes[5].set_ylabel('\n'.join(wrap(b.name, 16)), rotation=0, ha='right', va='center', fontsize='x-small')
+
+    axes[5].grid(which='both')
+    axes[5].set_facecolor(background)
+
+    axes[-1].set_xlabel(x.name)
+
+    fig.set_facecolor('white')
+
+    fig.tight_layout()
+    fig.subplots_adjust(hspace=0.05)
 
     fig.savefig("single_batch_sweep_latency.png")
 
