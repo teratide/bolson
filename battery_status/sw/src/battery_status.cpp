0f02410f324bba27fb436c2fd0e6d2b6de283e3b
Matthijs Brobbel <m1brobbel@gmail.com>
2020-11-11 11:18:17 +0100

    Update host app for dingen_vhd

--- battery_status/sw/src/battery_status.cpp
+++ battery_status/sw/src/battery_status.cpp
@@ -9,20 +9,72 @@
 
 #include <memory>
 #include <iostream>
+#include <bitset>
 
 #define AFU_GUID "9ca43fb0-c340-4908-b79b-5c89b4ef5eed";
 
 #ifdef NDEBUG
 #define PLATFORM "opae"
+#define ADDR_RANGE 4096
 #else
 #define PLATFORM "opae-ase"
+#define ADDR_RANGE 4
 #endif
 
-void write_dingen_reg(uint8_t reg, uint64_t value, Platform &platform)
+#define STATUS_LO ((0x98 - 0x40) / 4)
+#define STATUS_HI ((0x9c - 0x40) / 4)
+#define CONTROL_LO ((0xa0 - 0x40) / 4)
+#define CONTROL_HI ((0xa4 - 0x40) / 4)
+
+void write_dingen_reg(uint8_t reg, uint64_t value, fletcher::Platform &platform)
 {
-  platform.WriteMMIO(25, value);
-  platform.WriteMMIO(26, 0x20000000 | (reg << 27) | (value >> 32));
-  platform.WriteMMIO(26, 0);
+  platform.WriteMMIO(CONTROL_LO, value);
+  platform.WriteMMIO(CONTROL_HI, 0x20000000 | (reg << 27) | (value >> 32));
+  platform.WriteMMIO(CONTROL_HI, 0);
+}
+uint64_t read_dingen_checksum(fletcher::Platform &platform)
+{
+  platform.WriteMMIO(CONTROL_HI, 0x38000000);
+  uint32_t status_lo, status_hi;
+  platform.ReadMMIO(STATUS_LO, &status_lo);
+  platform.ReadMMIO(STATUS_HI, &status_hi);
+  return status_lo | ((uint64_t)status_hi << 32);
+}
+
+uint64_t read_dingen_pins(fletcher::Platform &platform)
+{
+  platform.WriteMMIO(CONTROL_HI, 0);
+  uint32_t status_lo, status_hi;
+  platform.ReadMMIO(STATUS_LO, &status_lo);
+  platform.ReadMMIO(STATUS_HI, &status_hi);
+  return status_lo | ((uint64_t)status_hi << 32);
+}
+
+uint64_t read_dingen_memory(uint32_t address, fletcher::Platform &platform)
+{
+  platform.WriteMMIO(CONTROL_HI, 0x40000000);
+  platform.WriteMMIO(CONTROL_LO, address);
+  uint32_t status_lo, status_hi;
+  platform.ReadMMIO(STATUS_LO, &status_lo);
+  platform.ReadMMIO(STATUS_HI, &status_hi);
+  return status_lo | ((uint64_t)status_hi << 32);
+}
+
+void capture_dingen(fletcher::Platform &platform)
+{
+  platform.WriteMMIO(CONTROL_HI, 0x80000000);
+}
+
+void wait_dingen_done(fletcher::Platform &platform)
+{
+  platform.WriteMMIO(CONTROL_HI, 0x80000000);
+  uint32_t status_hi;
+  platform.ReadMMIO(STATUS_HI, &status_hi);
+  while (!(status_hi & 0x80000000))
+  {
+    platform.ReadMMIO(STATUS_HI, &status_hi);
+  };
+  platform.WriteMMIO(CONTROL_HI, 0);
 }
 
 int main(int argc, char **argv)
@@ -115,10 +167,17 @@ int main(int argc, char **argv)
     return -1;
   }
 
-  write_dingen_reg(, , *platform);
+  write_dingen_reg(0, 0xFFFFFFFFFFFFFull, *platform); // capture mask
+
+  // set trigger for input_cmd valid
+  write_dingen_reg(1, 0x20000, *platform); // trigger mask
+  write_dingen_reg(2, 0x20000, *platform); // trigger match
+
+  printf("checksum: 0x%016llX\n", read_dingen_checksum(*platform));
+  capture_dingen(*platform);
 
   std::string mmio;
-  platform->MmioToString(&mmio, 0, 26);
+  platform->MmioToString(&mmio, 0, (0xa0 - 0x40) / 4);
   std::cout << mmio << std::endl;
 
   for (int i = 0; i < context->num_buffers(); i++)
@@ -144,10 +203,19 @@ int main(int argc, char **argv)
   {
     context->platform()->ReadMMIO(FLETCHER_REG_STATUS, &status_register);
     std::cerr << "status: " << status_register << std::endl;
-    platform->MmioToString(&mmio, 0, 26);
+    platform->MmioToString(&mmio, 0, (0xa0 - 0x40) / 4);
     std::cout << mmio << std::endl;
     sleep(1);
     done = (status_register & 1ul << FLETCHER_REG_STATUS_DONE) == 1ul << FLETCHER_REG_STATUS_DONE;
+  }
+
+  // wait_dingen_done(*platform);
+  size_t cycle = -2;
+  for (size_t addr = 0; addr < ADDR_RANGE; addr++)
+  {
+    uint64_t data = read_dingen_memory(addr, *platform);
+    cycle += 1 + (data >> 52);
+    printf("cycle %10d: %s\n", cycle, std::bitset<52>(data & 0xFFFFFFFFFFFFFull).to_string().c_str());
   }
 
   // uint32_t return_value_0;
@@ -169,7 +237,7 @@ int main(int argc, char **argv)
     std::cout << view.ToString() << std::endl;
   }
 
-  platform->MmioToString(&mmio, 0, 26);
+  platform->MmioToString(&mmio, 0, (0xa0 - 0x40) / 4);
   std::cout << mmio << std::endl;
 
   std::cout << output_batch.get()->ToString() << std::endl;
