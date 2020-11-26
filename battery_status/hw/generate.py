790773a2e6b82352b4c49ddaa201ca6103d352e6
Matthijs Brobbel <m1brobbel@gmail.com>
2020-11-26 10:33:48 +0100

    Add illex metadata to output schema of battery status example

--- battery_status/hw/generate.py
+++ battery_status/hw/generate.py
@@ -19,11 +19,14 @@ with pa.RecordBatchFileWriter('in.rb', input_schema) as writer:
 
 output_schema = pa.schema([
     pa.field("voltage", pa.list_(
-        pa.field("item", pa.uint64(), False)
+        pa.field("item", pa.uint64(), False).with_metadata(
+            {"illex_MIN": "0", "illex_MAX": "2047"})
     ), False)
 ]).with_metadata({
     b'fletcher_mode': b'write',
-    b'fletcher_name': b'output'
+    b'fletcher_name': b'output',
+    "illex_MIN_LENGTH": "1",
+    "illex_MAX_LENGTH": "16"
 })
 
 pa.output_stream("out.as").write(output_schema.serialize())
