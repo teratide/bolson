e159c4109661474096fdf6d733cc8f67bd556db4
Matthijs Brobbel <m1brobbel@gmail.com>
2020-11-26 17:35:17 +0100

    Add throughput test

--- battery_status/sw/src/latency.cpp
+++ battery_status/sw/src/latency.cpp
@@ -13,9 +13,11 @@
 
 #define AFU_GUID "9ca43fb0-c340-4908-b79b-5c89b4ef5eed";
 #define PLATFORM "opae"
+#define MULTIPLE 10
 
 static const std::string TINY_RECORD("{\"voltage\": [1]}");
 static const std::string SMALL_RECORD("{\"voltage\": [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]}");
+static const std::string LARGE_RECORD("{\"voltage\": [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]}");
 
 template <typename Clock = std::chrono::high_resolution_clock>
 class Timer
@@ -84,7 +86,7 @@ int main(int argc, char **argv)
   // open fpga handle
   Timer<> open_fpga_handle;
   Timer<> close_fpga_handle;
-  for (int i = 0; i < 1000; i++)
+  for (int i = 0; i < MULTIPLE; i++)
   {
 
     open_fpga_handle.start();
@@ -110,7 +112,7 @@ int main(int argc, char **argv)
   Timer<> read_mmio;
   Timer<> write_mmio;
   Timer<> read_write_read_mmio;
-  for (int i = 0; i < 1000; i++)
+  for (int i = 0; i < MULTIPLE; i++)
   {
     read_write_read_mmio.start();
 
@@ -130,8 +132,10 @@ int main(int argc, char **argv)
   write_mmio.print("Write MMIO");
   read_write_read_mmio.print("RWR MMIO");
 
+  platform->Terminate();
+
   // allocate output buffers
-  size_t buffer_size = 4096;
+  size_t buffer_size = 16 * 1024 * 1024;
   uint8_t *offset_data = (uint8_t *)memalign(sysconf(_SC_PAGESIZE), buffer_size);
   memset(offset_data, 0, buffer_size);
   auto offset_buffer = std::make_shared<arrow::Buffer>(offset_data, buffer_size);
@@ -142,38 +146,145 @@ int main(int argc, char **argv)
   std::vector<std::shared_ptr<arrow::Array>> arrays = {list_array};
   auto output_batch = arrow::RecordBatch::Make(output_schema(), 0, arrays);
 
-  // create input record batches
+  // tiny record
   arrow::UInt8Builder tiny_record_builder;
   tiny_record_builder.AppendValues({TINY_RECORD.begin(), TINY_RECORD.end()});
   std::shared_ptr<arrow::Array> tiny_record_array;
   tiny_record_builder.Finish(&tiny_record_array);
-  auto tiny_record_batch = arrow::RecordBatch::Make(input_schema(), 1, {tiny_record_array});
+  auto tiny_record_batch = arrow::RecordBatch::Make(input_schema(), TINY_RECORD.size(), {tiny_record_array});
 
-  arrow::UInt8Builder small_record_builder;
-  small_record_builder.AppendValues({SMALL_RECORD.begin(), SMALL_RECORD.end()});
-  std::shared_ptr<arrow::Array> small_record_array;
-  small_record_builder.Finish(&small_record_array);
-  auto small_record_batch = arrow::RecordBatch::Make(input_schema(), 1, {small_record_array});
-
-  // create context
+  fletcher::Platform::Make(PLATFORM, &platform, false);
+  platform->init_data = &guid;
+  platform->Init();
   std::shared_ptr<fletcher::Context> context;
   fletcher::Context::Make(&context, platform);
-
-  // tiny record test
   context->QueueRecordBatch(tiny_record_batch);
   context->QueueRecordBatch(output_batch);
   context->Enable();
-
+  std::cerr << std::endl;
   fletcher::Kernel kernel(context);
   kernel.WriteMetaData();
 
   Timer<> tiny_record;
-  for (int i = 0; i < 1000; i++)
+  Timer<> kernel_reset;
+
+  for (int i = 0; i < MULTIPLE; i++)
   {
     tiny_record.start();
     kernel.Start();
     kernel.PollUntilDone();
     tiny_record.stop();
+
+    kernel_reset.start();
+    kernel.Reset();
+    kernel_reset.stop();
   }
+
   tiny_record.print("Tiny record");
+  std::cerr << TINY_RECORD.size() / tiny_record.time() << " bytes per second" << std::endl;
+
+  kernel_reset.print("Kernel reset");
+  platform->Terminate();
+
+  // small record
+  arrow::UInt8Builder small_record_builder;
+  small_record_builder.AppendValues({SMALL_RECORD.begin(), SMALL_RECORD.end()});
+  std::shared_ptr<arrow::Array> small_record_array;
+  small_record_builder.Finish(&small_record_array);
+  auto small_record_batch = arrow::RecordBatch::Make(input_schema(), SMALL_RECORD.size(), {small_record_array});
+
+  fletcher::Platform::Make(PLATFORM, &platform, false);
+  platform->init_data = &guid;
+  platform->Init();
+  fletcher::Context::Make(&context, platform);
+  context->QueueRecordBatch(small_record_batch);
+  context->QueueRecordBatch(output_batch);
+  context->Enable();
+  std::cerr << std::endl;
+  kernel = fletcher::Kernel(context);
+  kernel.WriteMetaData();
+
+  Timer<> small_record;
+
+  for (int i = 0; i < MULTIPLE; i++)
+  {
+    small_record.start();
+    kernel.Start();
+    kernel.PollUntilDone();
+    small_record.stop();
+    kernel.Reset();
+  }
+
+  small_record.print("Small record");
+  std::cerr << SMALL_RECORD.size() / small_record.time() << " bytes per second" << std::endl;
+  platform->Terminate();
+
+  // large record
+  arrow::UInt8Builder large_record_builder;
+  large_record_builder.AppendValues({LARGE_RECORD.begin(), LARGE_RECORD.end()});
+  std::shared_ptr<arrow::Array> large_record_array;
+  large_record_builder.Finish(&large_record_array);
+  auto large_record_batch = arrow::RecordBatch::Make(input_schema(), LARGE_RECORD.size(), {large_record_array});
+
+  fletcher::Platform::Make(PLATFORM, &platform, false);
+  platform->init_data = &guid;
+  platform->Init();
+  fletcher::Context::Make(&context, platform);
+  context->QueueRecordBatch(large_record_batch);
+  context->QueueRecordBatch(output_batch);
+  context->Enable();
+  std::cerr << std::endl;
+  kernel = fletcher::Kernel(context);
+  kernel.WriteMetaData();
+
+  Timer<> large_record;
+
+  for (int i = 0; i < MULTIPLE; i++)
+  {
+    large_record.start();
+    kernel.Start();
+    kernel.PollUntilDone();
+    large_record.stop();
+    kernel.Reset();
+  }
+
+  large_record.print("Large record");
+  std::cerr << LARGE_RECORD.size() / large_record.time() << " bytes per second" << std::endl;
+  platform->Terminate();
+
+  // multiple tiny record
+  arrow::UInt8Builder multiple_tiny_record_builder;
+  for (int i = 0; i < MULTIPLE; i++)
+  {
+    multiple_tiny_record_builder.AppendValues({TINY_RECORD.begin(), TINY_RECORD.end()});
+  }
+  std::shared_ptr<arrow::Array> multiple_tiny_record_array;
+  multiple_tiny_record_builder.Finish(&multiple_tiny_record_array);
+  auto multiple_tiny_record_batch = arrow::RecordBatch::Make(input_schema(), MULTIPLE * TINY_RECORD.size(), {multiple_tiny_record_array});
+
+  fletcher::Platform::Make(PLATFORM, &platform, false);
+  platform->init_data = &guid;
+  platform->Init();
+  fletcher::Context::Make(&context, platform);
+  context->QueueRecordBatch(multiple_tiny_record_batch);
+  context->QueueRecordBatch(output_batch);
+  context->Enable();
+  std::cerr << std::endl;
+  kernel = fletcher::Kernel(context);
+  kernel.WriteMetaData();
+
+  Timer<> multiple_tiny_record;
+
+  for (int i = 0; i < MULTIPLE; i++)
+  {
+    multiple_tiny_record.start();
+    kernel.Start();
+    kernel.PollUntilDone();
+    multiple_tiny_record.stop();
+    kernel.Reset();
+  }
+
+  multiple_tiny_record.print("Multiple Tiny record");
+  std::cerr << MULTIPLE * TINY_RECORD.size() / multiple_tiny_record.time() << " bytes per second" << std::endl;
+  platform->Terminate();
 }
