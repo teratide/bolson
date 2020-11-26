e3074086503d09918cda9f7185eec1d97178d53d
Matthijs Brobbel <m1brobbel@gmail.com>
2020-11-26 21:01:00 +0100

    Set illex metadata on list field

--- battery_status/hw/generate.py
+++ battery_status/hw/generate.py
@@ -21,12 +21,12 @@ output_schema = pa.schema([
     pa.field("voltage", pa.list_(
         pa.field("item", pa.uint64(), False).with_metadata(
             {"illex_MIN": "0", "illex_MAX": "2047"})
-    ), False)
+    ), False).with_metadata(
+        {"illex_MIN_LENGTH": "1", "illex_MAX_LENGTH": "16"}
+    )
 ]).with_metadata({
     b'fletcher_mode': b'write',
-    b'fletcher_name': b'output',
-    "illex_MIN_LENGTH": "1",
-    "illex_MAX_LENGTH": "16"
+    b'fletcher_name': b'output'
 })
 
 pa.output_stream("out.as").write(output_schema.serialize())
