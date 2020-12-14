2e0baf6dcddbe594c469105601eff73f541cfbbe
Matthijs Brobbel <m1brobbel@gmail.com>
2020-12-14 16:52:34 +0100

    Update application

--- battery_status_2/hw/generate.py
+++ battery_status_2/hw/generate.py
@@ -9,10 +9,34 @@ pa.output_stream("in_1.as").write(input_schema.with_metadata({
     b'fletcher_mode': b'read',
     b'fletcher_name': b'input_1'
 }).serialize())
+
 pa.output_stream("in_2.as").write(input_schema.with_metadata({
     b'fletcher_mode': b'read',
     b'fletcher_name': b'input_2'
 }).serialize())
+
+with pa.RecordBatchFileWriter('in_1.rb', input_schema) as writer:
+    writer.write(
+        pa.RecordBatch.from_arrays(
+            [pa.array(
+                [byte for byte in '{"voltage": [1,2]}{"voltage": [4, 42]}'.encode()], pa.uint8())],
+            schema=input_schema.with_metadata({
+                b'fletcher_mode': b'read',
+                b'fletcher_name': b'input_1'
+            })
+        ))
+
+with pa.RecordBatchFileWriter('in_2.rb', input_schema) as writer:
+    writer.write(
+        pa.RecordBatch.from_arrays(
+            [pa.array(
+                [byte for byte in '{"voltage": [9,10]}'.encode()], pa.uint8())],
+            schema=input_schema.with_metadata({
+                b'fletcher_mode': b'read',
+                b'fletcher_name': b'input_2'
+            })
+        )
+    )
 
 output_schema = pa.schema([
     pa.field("voltage", pa.list_(
